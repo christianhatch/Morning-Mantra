3db016efc8058e112b843998a664bcc0f0aff323
Christian Hatch <christianhatch@gmail.com>
2014-07-23 01:05:48 -0400

    Initial Commit

---
+++ Morning Mantra/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Morning Mantra
+//
+//  Created by Christian Hatch on 7/23/14.
+//  Copyright (c) 2014 Knot Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Morning Mantra/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Morning Mantra
+//
+//  Created by Christian Hatch on 7/23/14.
+//  Copyright (c) 2014 Knot Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
